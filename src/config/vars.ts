14c1dddeb175d396347fefe46f683ed4940a0463
Jesús Genil <prgrav03@outsourcing.cepsa.com>
2021-06-13 16:27:19 +0200

    feat: add db data services

---
+++ src/config/vars.ts
@@ -0,0 +1,9 @@
+export default {
+    dataServicePaths: {
+        escalationPolicy: "escalation-policy/",
+        pager: "pager/"
+    },
+    baseUrls: {
+        dataServices: "https://db-connector-path/"
+    }
+}
